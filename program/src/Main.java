06e6492fee044a99c8527da5b373e2ee81d9fa2a
SMJSGaming <38814077+SMJSGaming@users.noreply.github.com>
2019-09-06 14:13:53 +0200

    Update Main.java

--- program/src/Main.java
+++ program/src/Main.java
@@ -4,13 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-/*
-TODO
--Make the final touches for the auto updater
--Auto folder generator
--Error stack detouring
- */
-
 public class Main extends JPanel {
     public static String currentdir = System.getProperty("user.dir");
     private static int curoffset = 0;
@@ -20,74 +13,78 @@ public class Main extends JPanel {
     private static ArrayList<JLabel> packs = new ArrayList<>();
 
     public static void main(String[] args) {
-        Updater.checkupdate();
-        if (currentpack.equals("Resources")) {
-            defaultmod.setText("Enabled");
-        }
-        Main jclass = new Main();
-        JFrame frame = new JFrame("Texture Pack Injector By SMJS");
-        String[] containers = InjectMod.getsubdirs(currentdir + "\\r\\");
-        if (containers.length != 0) {
-            int y = 60;
-            int x = 90;
-            for (int i = 0; i < containers.length; i++) {
-                if (i % 2 == 0 && i != 0) {
-                    y = 60;
-                    x += 220;
-                }
-                if (x <= 750) {
-                    packs.add(FrameUtils.packimg(x, y, 150, 150, containers, i));
-                    frame.add(packs.get(packs.size() - 1));
-                    packs.add(FrameUtils.packname(x, y, 150, 30, containers, i));
-                    frame.add(packs.get(packs.size() - 1));
-                    y += 220;
-                } else {
-                    packs.add(FrameUtils.packimg(0, 0, 0, 0, containers, i));
-                    frame.add(packs.get(packs.size() - 1));
-                    packs.add(FrameUtils.packname(0, 0, 0, 0, containers, i));
-                    frame.add(packs.get(packs.size() - 1));
-                }
-            }
-        }
-        curpack.setForeground(Color.WHITE);
-        curpack.setBounds(500, 5, 480, 25);
-        frame.add(curpack);
-
-        JButton previous = FrameUtils.buttonsfordays("Previous", 300, 490, 100, 30);
-        previous.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (curoffset > 0) {
-                    curoffset -= 16;
-                    FrameUtils.pageswitch(packs, curoffset);
-                }
-            }
-        });
-
-        defaultmod.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                InjectMod.moddefault(true);
+        PreLaunch.todo();
+        if (Updater.checkupdate()) {
+            Updater.Updater();
+        } else {
+            if (currentpack.equals("Resources")) {
                 defaultmod.setText("Enabled");
-                currentpack = InjectMod.checkpack();
-                curpack.setText("<html><h3>Current pack: " + currentpack + "</h3></html>");
             }
-        });
-
-        JButton next = FrameUtils.buttonsfordays("Next", 600, 490, 100, 30);
-        next.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (curoffset + 18 <= packs.size()) {
-                    curoffset += 16;
-                    FrameUtils.pageswitch(packs, curoffset);
+            Main jclass = new Main();
+            JFrame frame = new JFrame("Texture Pack Injector By SMJS");
+            String[] containers = InjectMod.getsubdirs(currentdir + "\\r\\");
+            if (containers.length != 0) {
+                int y = 60;
+                int x = 90;
+                for (int i = 0; i < containers.length; i++) {
+                    if (i % 2 == 0 && i != 0) {
+                        y = 60;
+                        x += 220;
+                    }
+                    if (x <= 750) {
+                        packs.add(FrameUtils.packimg(x, y, 150, 150, containers, i));
+                        frame.add(packs.get(packs.size() - 1));
+                        packs.add(FrameUtils.packname(x, y, 150, 30, containers, i));
+                        frame.add(packs.get(packs.size() - 1));
+                        y += 220;
+                    } else {
+                        packs.add(FrameUtils.packimg(0, 0, 0, 0, containers, i));
+                        frame.add(packs.get(packs.size() - 1));
+                        packs.add(FrameUtils.packname(0, 0, 0, 0, containers, i));
+                        frame.add(packs.get(packs.size() - 1));
+                    }
                 }
             }
-        });
+            curpack.setForeground(Color.WHITE);
+            curpack.setBounds(500, 5, 480, 25);
+            frame.add(curpack);
+
+            JButton previous = FrameUtils.buttonsfordays("Previous", 300, 490, 100, 30);
+            previous.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    if (curoffset > 0) {
+                        curoffset -= 16;
+                        FrameUtils.pageswitch(packs, curoffset);
+                    }
+                }
+            });
+
+            defaultmod.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    InjectMod.moddefault(true);
+                    defaultmod.setText("Enabled");
+                    currentpack = InjectMod.checkpack();
+                    curpack.setText("<html><h3>Current pack: " + currentpack + "</h3></html>");
+                }
+            });
+
+            JButton next = FrameUtils.buttonsfordays("Next", 600, 490, 100, 30);
+            next.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    if (curoffset + 18 <= packs.size()) {
+                        curoffset += 16;
+                        FrameUtils.pageswitch(packs, curoffset);
+                    }
+                }
+            });
 
 
-        frame.add(previous);
-        frame.add(defaultmod);
-        frame.add(next);
+            frame.add(previous);
+            frame.add(defaultmod);
+            frame.add(next);
 
-        FrameUtils.frameclose(frame, jclass, 1000, 600);
+            FrameUtils.frameclose(frame, jclass, 1000, 600);
+        }
     }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
